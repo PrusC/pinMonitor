2ced5002da2de57beb5a24e47ddc6182eebae418
PrusC <konstantin.prusakov@phystech.edu>
2020-03-22 20:49:59 +0300

    delete unused parameters

--- cmd.h
+++ cmd.h
@@ -8,26 +8,6 @@
 #ifndef CMD_H_
 #define CMD_H_
 
-//#define D0 0 //RX
-//#define D1 1 //TX
-//#define D2 2
-//#define D3 3
-//#define D4 4
-//#define D6 6
-//#define D7 7
-//#define D8 8
-//#define D9 9
-//#define D10 10
-//#define D11 11
-//#define D12 12
-//#define D13 13
-
-//#define A0 0
-//#define A1 1
-//#define A2 2
-//#define A3 3
-//#define A4 4
-
 #include "cmdParser.h"
 
 #define SET_MODE "SM"
